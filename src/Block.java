918af89c9bd717300d031109ddaf41c660fae1a7
Anuj <adharurkar@gmail.com>
2019-09-19 23:29:34 +0530

    Made some changes in hash-function Added generateFirstBlock() and add() methods.

--- src/Block.java
+++ src/Block.java
@@ -22,7 +22,7 @@ class Block {
     // Returns a Hex Hash value
     String calcHash() throws NoSuchAlgorithmException {
 
-        String msg = String.valueOf(blockId + String.valueOf(time) + transactions.size());
+        String msg = blockId + String.valueOf(time) + transactions.size();
 
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         final byte[] bytes = digest.digest(msg.getBytes());
@@ -49,4 +49,9 @@ class Block {
     public String getTime(){
         return time;
     }
+
+    public void setPrevHash(String prevHash) {
+        this.prevHash = prevHash;
+    }
+
 }
--- src/Blockchain.java
+++ src/Blockchain.java
@@ -1,3 +1,4 @@
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 class Blockchain {
@@ -5,12 +6,27 @@ class Blockchain {
     ArrayList<Transaction> pendingTransactions;
     private int difficulty = 0;
 
+    public Blockchain() throws NoSuchAlgorithmException {
+        chain = new ArrayList<>();
+        chain.add(generateFirstBlock());
+    }
+
+    private Block generateFirstBlock() throws NoSuchAlgorithmException {
+        Block generate = new Block("null",pendingTransactions,"27/05/1999",difficulty);
+        generate.setPrevHash(null);
+        generate.calcHash();
+        return generate;
+    }
     void setDifficulty(int diff){
         this.difficulty = diff;
     }
 
-    void add(){
+    void add(Block bk) throws NoSuchAlgorithmException {
         // Addition code for a new block
+
+        bk.setPrevHash(chain.get((int) (chain.size()-1.)).getCurrentHash());
+        bk.calcHash();
+        this.chain.add(bk);
     }
 
     boolean isValid(){
