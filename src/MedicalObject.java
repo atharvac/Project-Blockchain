c73acaeebff679c80c10337b2e656f7d16bf7890
KAMODCR7 <kamodlokhande777@hotmail.com>
2019-10-02 15:22:14 +0530

    constructors are added.

--- src/MedicalObject.java
+++ src/MedicalObject.java
@@ -9,13 +9,31 @@ class MedicalObject implements Serializable {
     // Basic Attributes of a medical object
     // Maybe this will be a base class for other specific classes, like Drugs, syringes etc.
     // This may be populated by a database.
+    MedicalObject(String type,String name,int quant,float amt){
+        this.TYPE = type;
+        this.NAME = name;
+        this.QUANTITY = quant;
+        this.AMOUNT = amt;
+    }
 }
 class MedicalHistory implements Serializable {
     String NAME;
     String ADDRESS;
     String BLOODGROUP;
     int AGE;
-    ArrayList<String> DIEASES;
+    ArrayList<String> DISEASES;
     ArrayList<String> ALLERGIES;
-
+    MedicalHistory(String name,String address,String bg,int age){
+        this.NAME = name;
+        this.ADDRESS = address;
+        this.BLOODGROUP = bg;
+        this.AGE = age;
+    }
+    void setDISEASES(ArrayList<String> d){
+        DISEASES = d;
+    }
+    void setALLERGIES(ArrayList<String>a){
+  ALLERGIES = a;
+    }
 }
+
