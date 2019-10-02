2e909ccf162b6efba2bd8c055034094ff66342c8
KAMODCR7 <kamodlokhande777@hotmail.com>
2019-10-02 15:03:02 +0530

    class Medical-history added.

--- src/MedicalObject.java
+++ src/MedicalObject.java
@@ -1,7 +1,21 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 class MedicalObject implements Serializable {
+    String TYPE;
+    String NAME;
+    int QUANTITY;
+    float AMOUNT;
     // Basic Attributes of a medical object
     // Maybe this will be a base class for other specific classes, like Drugs, syringes etc.
     // This may be populated by a database.
+}
+class MedicalHistory implements Serializable {
+    String NAME;
+    String ADDRESS;
+    String BLOODGROUP;
+    int AGE;
+    ArrayList<String> DIEASES;
+    ArrayList<String> ALLERGIES;
+
 }
